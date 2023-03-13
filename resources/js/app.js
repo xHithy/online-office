17a031aac53fb5161817c04fe21288f5a7ebb101
Hithy <hithyisgod@gmail.com>
2023-03-14 00:22:46 +0200

    Updated imports

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,2 +1,14 @@
+import $ from 'jquery';
 import './bootstrap';
 import '../css/main.scss';
+import './functions/selectAvatar';
+
+// Functions
+import { loadAvatars } from "./functions/loadAvatars";
+import { selectAvatar } from "./functions/selectAvatar";
+
+window.selectAvatar = selectAvatar;
+
+$( document ).ready(() => {
+    loadAvatars();
+});
