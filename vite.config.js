c1f28d24f5a44846179b2d165c95a57c773fc146
Hithy <hithyisgod@gmail.com>
2023-03-15 01:14:39 +0200

    Added plugin injection

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,17 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import inject from '@rollup/plugin-inject';
+
 
 export default defineConfig({
     plugins: [
         laravel({
             input: ['resources/js/app.js'],
             refresh: true,
+        }),
+        inject({
+            $: 'jquery',
+            jQuery: 'jquery',
         }),
     ],
 });
