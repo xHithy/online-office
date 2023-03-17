d9b6189a580df9718ceb4c8bfbaae7c24c6b5e66
Hithy <hithyisgod@gmail.com>
2023-03-17 19:00:17 +0200

    Added auth middleware to routes

--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,9 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::prefix('/v1/auth')->group(function() {
+   Route::post('/login', [UserController::class, 'login']);
+   Route::post('/logout', [UserController::class, 'logout'])->middleware('verify.user');
 });
--- routes/web.php
+++ routes/web.php
@@ -5,4 +5,4 @@ use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [UserController::class, 'index'])->name('landing');
-Route::get('/office', [RoomController::class, 'index'])->name('office');
+Route::get('/office', [RoomController::class, 'index'])->name('office')->middleware('verify.user');
