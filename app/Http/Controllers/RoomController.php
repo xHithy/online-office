9e78b5562f15fb535b6b6d525ba6966e076776fb
Hithy <hithyisgod@gmail.com>
2023-03-17 03:08:21 +0200

    Update RoomController.php

--- app/Http/Controllers/RoomController.php
+++ app/Http/Controllers/RoomController.php
@@ -2,12 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Room;
 use Illuminate\View\View;
 
 class RoomController extends Controller
 {
     public static function index(): View
     {
-        return view('office');
+        return view('office', [
+            'rooms' => Room::query()->get()
+        ]);
     }
 }
