d9cf65ab2d2b0ab61ceeef998cdc19551c5032c8
Hithy <hithyisgod@gmail.com>
2023-03-17 19:00:48 +0200

    Created login and logout handlers

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Support\Facades\Validator;
 use Illuminate\View\View;
 
 class UserController extends Controller
@@ -9,5 +12,38 @@ class UserController extends Controller
     public static function index(): View
     {
         return view('landing');
+    }
+
+    public static function login(): RedirectResponse
+    {
+        $validation = Validator::make(request()->all(), [
+           'name' => 'required|unique:users|min:2|max:20',
+           'avatar' => 'required|integer'
+        ]);
+
+        if($validation->fails()) {
+            return back()->withInput()->withErrors($validation->messages());
+        }
+
+        $user = User::query()->create([
+           'name' => request('name'),
+           'avatar_id' => request('avatar'),
+        ]);
+
+        // Set user ID as session variable so it can be used for validation/authentication
+        session(['user' => $user['id']]);
+
+        return redirect(route('office'));
+    }
+
+    public static function logout(): RedirectResponse
+    {
+        // Delete the user from the database
+        User::query()->where('id', session('user'))->delete();
+
+        // Remove the verification session
+        session()->forget('user');
+
+        return redirect(route('landing'));
     }
 }
